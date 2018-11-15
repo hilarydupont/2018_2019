b187cfd93913f8617e356b35da40daff6ae7a9c9
H <hilary.dupont@gmail.com>
2018-11-14 21:37:23 -0500

    push

---
+++ Wood/collection.js
@@ -0,0 +1,12 @@
+$(function() {  
+    $('.title').hover(function(){
+  $(this).toggleClass('clicked');
+});
+  });
+
+$(function() {  
+    $('.project').hover(function(){
+  $(this).toggleClass('hovering');
+});
+  });
+
